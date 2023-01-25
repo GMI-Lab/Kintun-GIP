7096963c3f7181ea843dbe447b7cdc9861947d6a
GMI-Lab <70078596+GMI-Lab@users.noreply.github.com>
2023-01-25 18:08:22 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,10 @@ setup(
     name='kintunGIP',
     version='0.9.0',
     packages=find_packages(include=["kintunGIP", "kintunGIP.*"]),
+    scripts=["kintunGIP/kintunGIP.py"],
     install_requires=[
-        'aragorn>=1.2.41',
-        'barrnap>=0.9',
         'biopython>=1.80',
         'glob2>=0.7',
-        'mmseqs2>=14.7e284',
-        'python>=3.10',
-        'sibeliaz>=1.2.5'
     ],
     url='https://github.com/GMI-Lab/Kintun-GIP',
     license='GNU GENERAL PUBLIC LICENSE',
