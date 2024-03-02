b2870c2dd3013f1a425f465c6af115dbac0d0187
Camilo Berríos-Pastén <33841622+cberriospasten@users.noreply.github.com>
2024-03-02 02:37:02 -0300

    Update check_fasta.py

--- kintunGIP/check_fasta.py
+++ kintunGIP/check_fasta.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep 29 12:54:10 2020
-Check if a given file is in FastA format
+Check if a given file is in genbank format
 @author: CBP
 """
 from Bio import SeqIO
@@ -10,7 +10,7 @@ from Bio import SeqIO
 
 def is_fasta(filename):
     with open(filename, "r") as handle:
-        fasta = SeqIO.parse(handle, "fasta")
+        fasta = SeqIO.parse(handle, "genbank")
         if not any(fasta):
             return False
         else:
@@ -18,4 +18,4 @@ def is_fasta(filename):
                 return False
             else:
                 return True
-    # False when `fasta` is empty or has more than one record
+    # False when `genbank` is empty or has more than one record
